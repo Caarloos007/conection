ee702787fa3375838aa618a18c7faba9bd111976
Caarloos007 <cmagrase@myuax.com>
2026-03-20 19:51:41 +0100

    .

---
+++ maestro.py
@@ -0,0 +1,10 @@
+# servidor
+from multiprocessing.connection import Listener
+
+listener = Listener(('localhost', 6000), authkey=b'contrasena')
+conn = listener.accept()
+
+data = conn.recv()
+conn.send(data.upper())
+
+conn.close()
